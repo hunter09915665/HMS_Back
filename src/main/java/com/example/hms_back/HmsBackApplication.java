c17793f4cecde8f818ecbe83853be4e51e8e0cd7
Hashan Pavithra <105833232+hunter09915665@users.noreply.github.com>
2025-07-20 09:21:34 +0530

    Add files via upload
    startup

---
+++ src/main/java/com/example/hms_back/HmsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.hms_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hms_back/HmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hms_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
